eaf964938b0a6e32dbbe0d00d00b4576e04f92ab
EWalles <ejomcdonald@gmail.com>
2022-08-30 16:20:17 -0400

    added javascript

--- server.js
+++ server.js
@@ -2,8 +2,15 @@
 const express = require("express")
 const app = express()
 const port = 3000
+const welcome = ["Welcome to Gitpub"]
 
+//NOTE: it must start with ./ if it's just a file, not an NPM package
 
+const drinks = require("./models/drinks.js") 
+
+app.get("/welcome", (req, res) => {
+    res.send(welcome)
+  })
 app.listen(port, () => {
   console.log("listening")
 })
