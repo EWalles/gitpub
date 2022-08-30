f3246cee456874b9ef90554864d6cf4e44238a45
EWalles <ejomcdonald@gmail.com>
2022-08-30 19:24:43 -0400

    drinks working

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
-// Require Dependencies
-const express = require("express")
-const welcome = ["Welcome to Gitpub"]
-const drinks = require("./models/drinks.js") 
-
-// initialize express app
-const app = express()
+const express = require('express')
+const app = require('liquid-express-views')(express())
 const port = 3000
+const drinks = require('./models/drinks.js')
 
-app.get("/", (req, res) => {
-    res.render("welcome")
-})
-app.get("/drinks", (req, res) => {
-  res.render("drinks_show.ejs")
+const reqLog = (req) => {
+    console.log('===========================')
+    console.log('this is the request object sent from the browser')
+    console.log(`${req.method} request sent to ${req.url}`)
+    console.log('req params are: ', req.params)
+    console.log('===========================')
+}
+
+app.get('/', (req, res) => {
+    reqLog(req)
+    res.send('Welcome to the Gitpub App!')
 })
 
-  app.get("/drinks/:id", (req, res) => {
-    res.render("drinks_show.ejs")
-  })
+// INDEX route for drinks 
+app.get('/drinks', (req, res) => {
+    // res.send(drinks)
+    res.render('index', {drink: drinks})
+})
+
+// SHOW route for drinks 
+
+app.get('drinks/:id', (req, res) => {
+    res.render('show', {drink: drinks[req.params.id]})
+})
 
 app.listen(port, () => {
-  console.log("listening")
+    console.log('server is running ', port)
 })
