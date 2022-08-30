becae41e8d3f42208eb7e4c930736df1351d0c67
EWalles <ejomcdonald@gmail.com>
2022-08-30 18:38:10 -0400

    updating drinks

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-
+// Require Dependencies
 const express = require("express")
-const app = express()
-const port = 3000
 const welcome = ["Welcome to Gitpub"]
-
-//NOTE: it must start with ./ if it's just a file, not an NPM package
-
 const drinks = require("./models/drinks.js") 
 
-app.get("/welcome", (req, res) => {
-    res.render("drinks_show.ejs")
+// initialize express app
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => {
+    res.render("welcome")
+})
+
+app.get("/drinks", (req, res) => {
+    res.render("drinks_index.ejs")
   })
+
 app.listen(port, () => {
   console.log("listening")
 })
