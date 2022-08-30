8067ef123870aeba2459ab1a9c5d9086464e7176
EWalles <ejomcdonald@gmail.com>
2022-08-30 19:18:28 -0400

    working on it

--- server.js
+++ server.js
@@ -10,12 +10,10 @@ const port = 3000
 app.get("/", (req, res) => {
     res.render("welcome")
 })
-
-app.get("/drinks/", (request, response) => {
-  response.render("drinks_index.ejs", {
-    allDrinks: drinks,
-  })
+app.get("/drinks", (req, res) => {
+  res.render("drinks_show.ejs")
 })
+
   app.get("/drinks/:id", (req, res) => {
     res.render("drinks_show.ejs")
   })
