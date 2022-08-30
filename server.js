7cbf5706c2961267597ef5069b13444dbaadf6c7
EWalles <ejomcdonald@gmail.com>
2022-08-30 19:28:58 -0400

    working

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
 const express = require('express')
-const app = require('liquid-express-views')(express())
+const app = express()
 const port = 3000
 const drinks = require('./models/drinks.js')
 
-const reqLog = (req) => {
-    console.log('===========================')
-    console.log('this is the request object sent from the browser')
-    console.log(`${req.method} request sent to ${req.url}`)
-    console.log('req params are: ', req.params)
-    console.log('===========================')
-}
 
-app.get('/', (req, res) => {
-    reqLog(req)
-    res.send('Welcome to the Gitpub App!')
-})
 
 // INDEX route for drinks 
 app.get('/drinks', (req, res) => {
@@ -25,7 +14,7 @@ app.get('/drinks', (req, res) => {
 // SHOW route for drinks 
 
 app.get('drinks/:id', (req, res) => {
-    res.render('show', {drink: drinks[req.params.id]})
+    res.render('show', {drink: drinks[req.pasrams.id]})
 })
 
 app.listen(port, () => {
