4a303de6778b8e56c33836c96f8d519bb414a50b
EWalles <ejomcdonald@gmail.com>
2022-08-31 08:11:15 -0400

    broken

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
 //require dependencies
 const express = require('express');
 const app = express();
-const drinks = require('./models/drinks.js');
+var drinks = require('./models/drinks.js');
 const port = 3000;
-app.set('view engine', 'ejs');
 
-
-
+//
 app.get('/', (req, res) => {
- 
     res.render('drinks_show.ejs')
 });
 
 // INDEX route for drinks 
-app.get('/drinks', (req, res) => {
+app.get('/drinks/', (req, res) => {
     // res.send(drinks)
-    res.render('index', {drink: drinks})
+    res.render('index', {allDrinks: drinks})
+    
 });
 
 // SHOW route for drinks 
 
-app.get('drinks/:id', (req, res) => {
-    res.render('show', {drink: drinks[req.params.id]})
+app.get('drinks/:drinks.js', (req, res) => {
+    res.render('show', {drinks: drinks[req.params.drinks]})
 });
 
 app.listen(port, () => {
