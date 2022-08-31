234d7179978cb6ae5177757d7c330fc42e1af2d0
EWalles <ejomcdonald@gmail.com>
2022-08-31 07:48:56 -0400

    working on it

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 //require dependencies
 const express = require('express');
 const app = express();
-const drinks = require('./models/drinks.js')
-const port = 3000
+const drinks = require('./models/drinks.js');
+const port = 3000;
+app.set('view engine', 'ejs');
 
 
 
 app.get('/', (req, res) => {
  
-    res.render('Welcome to the Gitpub App!')
-})
+    res.render('drinks_show.ejs')
+});
 
 // INDEX route for drinks 
 app.get('/drinks', (req, res) => {
     // res.send(drinks)
     res.render('index', {drink: drinks})
-})
+});
 
 // SHOW route for drinks 
 
 app.get('drinks/:id', (req, res) => {
     res.render('show', {drink: drinks[req.params.id]})
-})
+});
 
 app.listen(port, () => {
     console.log('server is running ', port)
-})
+});
